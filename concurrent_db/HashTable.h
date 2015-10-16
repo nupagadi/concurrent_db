da2e8e951c6eba40f05c7ca47784f542fdbbd010
iv.zaycev <iv.zaycev@tensor.ru>
2015-10-16 16:02:06 +0300

    templates

--- concurrent_db/HashTable.h
+++ concurrent_db/HashTable.h
@@ -6,13 +6,14 @@
 #include <deque>
 #include <string>
 #include <list>
+#include <algorithm>
 
-//template
+template<class key_t, class mapped_t>
 class HashTable
 {
 public:
-	typedef std::string key_t;
-	typedef std::string mapped_t;
+	//typedef std::string key_t;
+	//typedef std::string mapped_t;
 	typedef std::pair<key_t, mapped_t> elem_t;
 
 	HashTable() : mMap(HASH_TABLE_START_SIZE) {}
@@ -21,6 +22,7 @@ public:
 	mapped_t& Get(const key_t& key);
 
 	mapped_t& operator[](const key_t& key);
+	mapped_t& operator[](key_t&& key);
 
 private:
 	std::list<elem_t>& bucket(const key_t& key) { return mMap[std::hash<key_t>()(key) % mMap.size()]; }
@@ -30,3 +32,47 @@ private:
 
 
 };
+
+template<class key_t, class mapped_t>
+mapped_t& HashTable<key_t, mapped_t>::operator[](const key_t& key)
+{
+	auto& b_list = bucket(key);
+	auto it = std::find_if(b_list.begin(), b_list.end(), [&key](const elem_t& el){ return el.first == key; });
+	if (it != b_list.end())
+		return it->second;
+	else
+		return b_list.insert(it, { key, mapped_t() })->second;
+}
+
+template<class key_t, class mapped_t>
+mapped_t& HashTable<key_t, mapped_t>::operator[](key_t&& key)
+{
+	auto& b_list = bucket(key);
+	auto it = std::find_if(b_list.begin(), b_list.end(), [&key](const elem_t& el){ return el.first == key; });
+	if (it != b_list.end())
+		return it->second;
+	else
+		return b_list.insert(it, { key, mapped_t() })->second;
+}
+
+template<class key_t, class mapped_t>
+mapped_t& HashTable<key_t, mapped_t>::Get(const key_t& key)
+{
+	auto& b_list = bucket(key);
+	auto it = std::find_if(b_list.begin(), b_list.end(), [&key](const elem_t& el){ return el.first == key; });
+	if (it != b_list.end())
+		return it->second;
+	else
+		return b_list.insert(it, { key, mapped_t() })->second;
+}
+
+template<class key_t, class mapped_t>
+mapped_t& HashTable<key_t, mapped_t>::Set(const key_t& key, const mapped_t& value)
+{
+	auto& b_list = bucket(key);
+	auto it = std::find_if(b_list.begin(), b_list.end(), [&key](const elem_t& el){ return el.first == key; });
+	if (it != b_list.end())
+		return it->second = value;
+	else
+		return b_list.insert(it, { key, value })->second;
+}
--- concurrent_db/main.cpp
+++ concurrent_db/main.cpp
@@ -10,18 +10,15 @@ using namespace std;
 
 int main()
 {
-	auto p = make_pair(string("sdlfkj"), string("sdlffgbdfbdfkj"));
-	list<pair<string, string>> l;
-	cout << sizeof(p) << endl;
-	cout << sizeof(l) << endl;
-
-	std::deque<std::list<HashTable::elem_t>> mMap(64);
+	//auto p = make_pair(string("sdlfkj"), string("sdlffgbdfbdfkj"));
+	//list<pair<string, string>> l;
+	//cout << sizeof(p) << endl;
+	//cout << sizeof(l) << endl;
 
 
-	HashTable h_table;
-	cout << sizeof(mMap) << endl;
-	cout << (mMap.size()) << endl;
-	h_table["121"] = "121";
+	HashTable<string, string> h_table;
+
+	h_table[string("121")] = string("121");
 	h_table.Set("122", "122");
 	h_table.Set("123", "123");
 	h_table.Set("124", "124");
@@ -31,26 +28,26 @@ int main()
 	h_table.Set("128", "128");
 	h_table.Set("129", "129");
 	h_table.Set("120", "120");
-	h_table.Set("11", "11");
-	h_table.Set("12", "12");
-	h_table.Set("13", "13");
-	h_table.Set("14", "14");
-	h_table.Set("15", "15");
-	h_table.Set("16", "16");
-	h_table.Set("17", "17");
-	h_table.Set("18", "18");
-	h_table.Set("19", "19");
-	h_table.Set("10", "10");
-	h_table.Set("21", "21");
-	h_table.Set("22", "22");
-	h_table.Set("23", "23");
-	h_table.Set("24", "24");
-	h_table.Set("25", "25");
-	h_table.Set("26", "26");
-	h_table.Set("27", "27");
-	h_table.Set("28", "28");
-	h_table.Set("29", "29");
-	h_table.Set("20", "20");
+	//h_table.Set("11", "11");
+	//h_table.Set("12", "12");
+	//h_table.Set("13", "13");
+	//h_table.Set("14", "14");
+	//h_table.Set("15", "15");
+	//h_table.Set("16", "16");
+	//h_table.Set("17", "17");
+	//h_table.Set("18", "18");
+	//h_table.Set("19", "19");
+	//h_table.Set("10", "10");
+	//h_table.Set("21", "21");
+	//h_table.Set("22", "22");
+	//h_table.Set("23", "23");
+	//h_table.Set("24", "24");
+	//h_table.Set("25", "25");
+	//h_table.Set("26", "26");
+	//h_table.Set("27", "27");
+	//h_table.Set("28", "28");
+	//h_table.Set("29", "29");
+	//h_table.Set("20", "20");
 	h_table.Set("1", "1");
 	h_table.Set("2", "2");
 	h_table.Set("3", "3");
@@ -68,4 +65,22 @@ int main()
 	cout << h_table.Get("1") << endl;
 	cout << h_table["121"] << endl;
 	cout << h_table.Get("1230") << endl;
+
+	HashTable<int, string> htint;
+	htint[0] = "0";
+	htint[20] = "20";
+	htint[30] = "30";
+	cout << htint[0] << endl;
+	cout << htint[20] << endl;
+	cout << htint[30] << endl;
+	cout << htint[40] << endl;
+
+	HashTable<int, int> htint2;
+	htint2[2] = 3;
+	htint2[2] = 2;
+	htint2[0] = 0;
+	cout << htint2[0] << endl;
+	cout << htint2[2] << endl;
+	cout << htint2[3] << endl;
+	cout << htint2[40] << endl;
 }
